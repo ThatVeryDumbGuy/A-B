fce9a2f3e439604eca93a27637d517d1f98698f2
ThatVeryDumbGuy <94152261+ThatVeryDumbGuy@users.noreply.github.com>
2022-02-04 20:17:29 +0700

    Add files via upload

---
+++ A+B/src/APlusB.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class APlusB {
+	
+	public static void main (String[] args) {
+		String[] input;
+		int A, B;
+		Scanner sc = new Scanner(System.in);
+		
+		input = sc.nextLine().split(" ");
+		A = Integer.parseInt(input[0]);
+		B = Integer.parseInt(input[1]);
+		
+		System.out.println(A + B);
+		}
+}
